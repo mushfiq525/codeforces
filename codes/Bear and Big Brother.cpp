c6fc5e627805a7ae9c9d620395f4bd08355ee2e6
Mushfiqur Rahman <mushfiq181250@gmail.com>
2022-08-16 09:52:37 +0600

    Add files via upload

---
+++ codes/Bear and Big Brother.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+
+    int a,b,c=0;
+    cin>>a>>b;
+    while(a<=b)
+    {
+        a*=3; b*=2;
+        c++;
+    }
+    cout<<c<<endl;
+
+    return 0;
+}
